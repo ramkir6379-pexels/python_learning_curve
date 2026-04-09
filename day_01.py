1cd429ce795067b800285140811b3afc093cacd5
ramkir6379-pexels <ramkir6379@gmail.com>
2026-04-09 14:43:12 +0530

    initial day 01 commit

---
+++ day_01.py
@@ -0,0 +1,17 @@
+# Learninig python with W3Schools
+# https://www.w3schools.com/python/default.asp
+# Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+# It is used for:
+# - web development (server-side),
+# - software development,   
+# - mathematics, and
+# - system scripting.
+# Python is a programming language that lets you work quickly and integrate systems more effectively.
+# Python is a case-sensitive language.
+# Variables are containers for storing data values.
+# Python has no command for declaring a variable.
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
